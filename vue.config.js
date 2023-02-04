e7d9ffe5bbc3e7c5123d71cc77ef8301a285cb26
Jonathan Gomes <69915159+Jonathan-Gomes-Sousa@users.noreply.github.com>
2023-02-04 18:12:27 -0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'/pedido-cidade',
+  publicPath:'/pedido-cidade/',
   transpileDependencies: true
 })
